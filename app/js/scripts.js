683355f170643e2054adeb71045792043fd218d1
xkored <xkored@gmail.com>
2019-07-05 14:56:31 +0400

    finished main page

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -7,6 +7,33 @@ $(function() {
       dots: true,
       navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"]
 	});
+  $('.trust__in').owlCarousel({
+      nav: true,
+      items: 5,
+      loop: true,
+      smartSpeed: 800,
+      margin: 20,
+      dots: false,
+      navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
+      responsive : {
+        0   : {
+            items: 1
+        },
+        380 : {
+            items: 2
+        },
+        480 : {
+            items: 3
+        },
+        768 : {
+            items: 4
+        },
+        1040 : {
+            items: 5
+        }
+      }
+  });
+  
 	// tabs 
   $(document).ready(function () {
     $(".tabs-content__item:not(:first-child)").hide();
@@ -24,4 +51,74 @@ $(function() {
   // $(document).on('af_complete', function(event,res) {
   //   if(res.success) parent.$.fancybox.close();
   // });
+
+  //Стилизованный input type file
+  $(".upload__file").change(function() {
+    var filename = [];
+    for (var i = 0; i < $(this).get(0).files.length; ++i) { // Запускаем цикл и перебираем все файлы
+      filename.push($(this).get(0).files[i].name.slice(0, 18) + '...'); // Добавляем имена файлов в массив
+    }
+    $(this).next(".filename").text(filename.join(", "));
+    $(this).parent().next('.upload__file-status').css('display','none');
+  });
+
+  //map
+  $(document).ready(function () {
+    var myMap;
+
+    $('.js-tab-contact').on('click', function(){
+        if (!$(this).hasClass('contact__item_active')) {
+            $(this).addClass('contact__item_active').siblings().removeClass('contact__item_active')
+            var arCoord = $(this).data('coord');
+            myMap.panTo(arCoord, {
+                delay: 3500
+            });
+        }
+    });
+
+    ymaps.ready(init);
+    function init(){
+      myMap = new ymaps.Map("ymap_tabs", {
+          center: [59.86663456, 30.47202250],
+          zoom: 16
+      });
+
+      var placemarks = [
+          {
+              coords: [59.86663456, 30.47202250],
+              icon: '/img/baloon.png',
+              icon_size: [46, 52],
+              icon_offset: [-23, -52], // -50% ширины, -100% высоты от точки привязки (левый верхний угол)
+          },
+          {
+              coords: [59.82384206, 30.52547200],
+              icon: '/img/baloon.png',
+              icon_size: [46, 52],
+              icon_offset: [-23, -52],
+          },
+          {
+              coords: [60.54950006, 30.21664900],
+              icon: '/img/baloon.png',
+              icon_size: [46, 52],
+              icon_offset: [-23, -52],
+          }
+      ];
+
+      placemarks.forEach(function(item){
+          var obj = new ymaps.Placemark(
+              item.coords,
+              {},
+              {
+                  iconLayout: 'default#image',
+                  iconImageHref: item.icon,
+                  iconImageSize: item.icon_size,
+                  iconImageOffset: item.icon_offset,
+              }
+          );
+          myMap.geoObjects.add(obj);
+      });
+    };
+  });
+
+
 });
