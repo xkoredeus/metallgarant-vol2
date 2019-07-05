e3261832ea5e362bfcb7ae7d9c99ab628d575a02
xkored <xkored@gmail.com>
2019-07-05 16:36:21 +0400

    created rev + corp pages

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -33,6 +33,57 @@ $(function() {
         }
       }
   });
+  $('.rev__in').owlCarousel({
+      nav: true,
+      items: 5,
+      loop: true,
+      smartSpeed: 800,
+      margin: 20,
+      dots: false,
+      navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
+      responsive : {
+        0   : {
+            items: 1
+        },
+        380 : {
+            items: 2
+        },
+        480 : {
+            items: 3
+        },
+        768 : {
+            items: 4
+        },
+        1040 : {
+            items: 5
+        }
+      }
+  });
+  $('.corp__in').owlCarousel({
+      nav: true,
+      loop: true,
+      smartSpeed: 800,
+      margin: 20,
+      dots: false,
+      navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
+      responsive : {
+        0   : {
+            items: 1
+        },
+        380 : {
+            items: 1
+        },
+        480 : {
+            items: 1
+        },
+        768 : {
+            items: 2
+        },
+        1040 : {
+            items: 3
+        }
+      }
+  });
   
 	// tabs 
   $(document).ready(function () {
