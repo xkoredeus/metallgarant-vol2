3e26a356de4a20121863a4caf1bddc598773c681
xkored <xkored@gmail.com>
2019-07-15 16:08:46 +0400

    created metallprokat card page

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -244,6 +244,25 @@ $(function() {
         }
       }
   });
+  $('.hot__in').owlCarousel({
+      nav: true,
+      loop: true,
+      smartSpeed: 800,
+      margin: 20,
+      dots: false,
+      navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
+      responsive : {
+        0   : {
+            items: 1
+        },
+        768 : {
+            items: 2
+        },
+        1040 : {
+            items: 3
+        }
+      }
+  });
   //catalog list|tab view
   $('.roll__view').on('click', function(){
     var view = $(this).attr('data-view');
@@ -253,18 +272,25 @@ $(function() {
     $('.roll__list').removeClass('view__col view__row').addClass(view);
   });
 	// tabs 
-  $(document).ready(function () {
-    $(".tabs-content__item:not(:first-child)").hide();
-    $(".tabs__container div.tabs__content-item.active-tab").show();
-    $('ul.tabs__list > li').click(function () {
-      if (!($(this).hasClass('active'))) {
-        var thisLi = $(this);
-        var numLi = thisLi.index();
-        thisLi.addClass('active').siblings().removeClass('active');
-        thisLi.parent().next().children('div').hide().eq(numLi).fadeIn('slow');
-      }
+  if ( $(window).width() > 600 ) {
+    $(document).ready(function () {
+      $(".tabs__content-item:not(:first-child)").hide();
+      $(".tabs__container div.tabs__content-item.active-tab").show();
+      $('ul.tabs__list > li').click(function () {
+        if (!($(this).hasClass('active'))) {
+          var thisLi = $(this);
+          var numLi = thisLi.index();
+          thisLi.addClass('active').siblings().removeClass('active');
+          thisLi.parent().next().children('div').hide().eq(numLi).fadeIn('slow');
+        }
+      });
     });
-  });
+  };
+  if ( $(window).width() < 600 ) {
+    $('.tabs-acc__btn').on('click', function(){
+      $(this).toggleClass('active').siblings('.tabs-acc__cnt').slideToggle();
+    })
+  };
   //Закрываем AjaxForm popup после успешной отправки
   // $(document).on('af_complete', function(event,res) {
   //   if(res.success) parent.$.fancybox.close();
@@ -279,7 +305,98 @@ $(function() {
     $(this).next(".filename").text(filename.join(", "));
     $(this).parent().next('.upload__file-status').css('display','none');
   });
+  //roll page slider
+  var sync1 = $('.roll-page__img-top');
+  var sync2 = $('.roll-page__img-bot');
 
+  var thumbnailItemClass = '.owl-item';
+
+  var slides = sync1.owlCarousel({
+    // video:true,
+    startPosition: 1,
+    items:1,
+    loop:true,
+    margin:10,
+    nav: true,
+    navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
+    dots: false,
+    smartSpeed: 700,
+    animateIn: 'fadeIn',
+    animateOut: 'fadeOut',
+    autoplay:false,
+    autoplayTimeout:6000,
+    autoplayHoverPause:false,
+  }).on('changed.owl.carousel', syncPosition);
+
+  function syncPosition(el) {
+    $owl_slider = $(this).data('owl.carousel');
+    var loop = $owl_slider.options.loop;
+
+    if(loop){
+      var count = el.item.count-1;
+      var current = Math.round(el.item.index - (el.item.count/2) - .5);
+      if(current < 0) {
+          current = count;
+      }
+      if(current > count) {
+          current = 0;
+      }
+    }else{
+      var current = el.item.index;
+    }
+
+    var owl_thumbnail = sync2.data('owl.carousel');
+    var itemClass = "." + owl_thumbnail.options.itemClass;
+
+
+    var thumbnailCurrentItem = sync2
+    .find(itemClass)
+    .removeClass("synced")
+    .eq(current);
+
+    thumbnailCurrentItem.addClass('synced');
+
+    if (!thumbnailCurrentItem.hasClass('active')) {
+      var duration = 300;
+      sync2.trigger('to.owl.carousel',[current, duration, true]);
+    }   
+  }
+  var thumbs = sync2.owlCarousel({
+    startPosition: 1,
+    items:4,
+    loop:false,
+    margin: 15,
+    autoplay:false,
+    navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
+    dots: false,
+    responsive : {
+      0   : {
+          items: 3,
+          // autoHeight: true
+      },
+      768 : {
+          items: 4,
+      },
+      1040 : {
+          items: 5,
+          mouseDrag: false
+      }
+    },
+    onInitialized: function (e) {
+      var thumbnailCurrentItem =  $(e.target).find(thumbnailItemClass).eq(this._current);
+      thumbnailCurrentItem.addClass('synced');
+    },
+  })
+  .on('click', thumbnailItemClass, function(e) {
+      e.preventDefault();
+      var duration = 300;
+      var itemIndex =  $(e.target).parents(thumbnailItemClass).index();
+      sync1.trigger('to.owl.carousel',[itemIndex, duration, true]);
+  }).on("changed.owl.carousel", function (el) {
+    var number = el.item.index;
+    $owl_slider = sync1.data('owl.carousel');
+    $owl_slider.to(number, 100, true);
+  });
   //map
   $(document).ready(function () {
     var myMap;
@@ -394,4 +511,20 @@ $(function() {
       $(this).toggleClass('active').next('div').find('.footer__link').slideToggle();
     });
   };
+  //filter float submit
+  // $('.begin__nav_filter select').on('change', function() {
+  //   console.log($(this).position());
+  // });
+  // $('.begin__nav_filter input').on('change', function() {
+  //   console.log($(this).offset());
+  // });
+  //input type number
+  $( '.quantity' ).on( 'click', '.quantity-minus, .quantity-plus', function () {
+  var input = $( this ).siblings( '.quantity-num' );
+    if ( (input.val() > 1) && ($( this ).hasClass( 'quantity-minus' ) ) ) {
+        input.val( +input.val() - 1 );
+    } else if ( $( this ).hasClass( 'quantity-plus' ) ) {
+      input.val( +input.val() + 1 );
+    };
+  });
 });
